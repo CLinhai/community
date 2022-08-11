849de5dd208b8a5b5266779a597e08b46a614614
CLinhai <git6425@163.com>
2022-08-11 21:43:20 +0800

    邮箱激活注册

---
+++ src/test/java/com/nowcoder/community/MaliTests.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * Created by lh on 2022/8/10
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MaliTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("1371198177@qq.com", "TEST", "Welcome.");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1371198177@qq.com","HTML",content);
+    }
+}
