860bec35e38eabd88eeade89c707a75c20fd721f
CLinhai <git6425@163.com>
2022-08-15 19:27:23 +0800

    统一异常处理-统一日志管理-第三章完成

---
+++ src/main/java/com/nowcoder/community/aspect/TestAspect.java
@@ -0,0 +1,49 @@
+package com.nowcoder.community.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Created by lh on 2022/8/15
+ */
+
+//@Component
+//@Aspect
+public class TestAspect {
+
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterRetuning() {
+        System.out.println("afterRetuning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+
+}
--- src/test/java/com/nowcoder/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -2,9 +2,11 @@ package com.nowcoder.community;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.LoginTicketMapper;
+import com.nowcoder.community.dao.MessageMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.LoginTicket;
+import com.nowcoder.community.entity.Message;
 import com.nowcoder.community.entity.User;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +32,8 @@ public class MapperTests {
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testSelectUser() {
@@ -116,5 +120,28 @@ public class MapperTests {
         post.setScore(80.0);
         int num = discussPostMapper.insertDiscussPost(post);
         System.out.println(num);
+    }
+
+    @Test
+    public void testSelectLetters() {
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
+
     }
 }
